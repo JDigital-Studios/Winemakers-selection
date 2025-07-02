fe647cc20c55428b5cf1a38a3f94d6efb643338c
Jamison Wight <jamisonw.wight@gmail.com>
2025-07-02 20:55:48 +0000

    Update copyright address

--- wp-content/themes/wms-theme/footer.php
+++ wp-content/themes/wms-theme/footer.php
@@ -12,7 +12,7 @@
 				<a href="mailto:info@winemakersselection.com?subject=Winemakers%20Selection%20Wines" class="btn" target="_blank" role="button" aria-label="opens in a new tab">Contact Us</a>
 				<div class="footer_nav" role="navigation">
 					<?php joints_footer_links(); ?>	
-					<p>© <?php echo date("Y"); ?> WINEMAKERS SELECTION WINES, LIVERMORE, CA</p>
+					<p>© <?php echo date("Y"); ?> WINEMAKERS SELECTION WINES, RIPON, CA</p>
 				</div>
 			</footer>
 		</div> <!-- end .off-canvas-wrapper -->
